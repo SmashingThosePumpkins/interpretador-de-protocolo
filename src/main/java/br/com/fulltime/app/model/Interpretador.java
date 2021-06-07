eabbd34243a6d4b1f3e41f764ef353c63cdd173c
Pablo A <pablo.silva@ftdata.com.br>
2021-06-07 09:04:04 -0300

    Modificações no Interpretador

--- src/main/java/br/com/fulltime/app/model/Interpretador.java
+++ src/main/java/br/com/fulltime/app/model/Interpretador.java
@@ -4,6 +4,7 @@ public class Interpretador {
 
     final int COD_AUTENTICACAO = 33;
     final int COD_EVENTO = 36;
+    final int COD_STATUS = 86;
     String[] array;
 
     public Interpretador(String[] array) {
@@ -32,6 +33,10 @@ public class Interpretador {
                 var evento = new Evento();
                 return evento.getDados(array);
             }
+            case COD_STATUS -> {
+                var status = new Status();
+                return status.getDados(array);
+            }
             default -> {
                 return null;
             }
@@ -42,29 +47,47 @@ public class Interpretador {
         var dados = getDados();
         String dadosFormatados;
         switch (getComando()) {
-            case COD_AUTENTICACAO ->
-                dadosFormatados = String.format(
-                        """
-                        NUMERO DE SERIE = %s
-                        IMEI = %s
-                        MAC = %s
-                        MODELO = %s
-                        VERSÃO = %s
-                        IP = %s
-                        SIMCARD = %s
-                        VIA = %s
-                        OPERADORA = %s""", dados[0], dados[1], dados[2], dados[3], dados[4], dados[5], dados[6], dados[7], dados[8]);
-            case COD_EVENTO ->
-                dadosFormatados = String.format(
-                        """
-                        CONTA = %s
-                        QUALIFICADOR = %s
-                        EVENTO = %s
-                        PARTIÇÃO = %s
-                        ARGUMENTO = %s
-                        CONTADOR = %s
-                        STATUS PART. = %s
-                        PROBLEMA = %s""", dados[0], dados[1], dados[2], dados[3], dados[4], dados[5], dados[6], dados[7]);
+            case COD_AUTENTICACAO -> dadosFormatados = String.format("""
+                    NUMERO DE SERIE = %s
+                    IMEI = %s
+                    MAC = %s
+                    MODELO = %s
+                    VERSÃO = %s
+                    IP = %s
+                    SIMCARD = %s
+                    VIA = %s
+                    OPERADORA = %s""", dados[0], dados[1], dados[2], dados[3], dados[4], dados[5], dados[6], dados[7], dados[8]);
+            case COD_EVENTO -> dadosFormatados = String.format("""
+                    CONTA = %s
+                    QUALIFICADOR = %s
+                    EVENTO = %s
+                    PARTIÇÃO = %s
+                    ARGUMENTO = %s
+                    CONTADOR = %s
+                    STATUS PART. = %s
+                    PROBLEMA = %s""", dados[0], dados[1], dados[2], dados[3], dados[4], dados[5], dados[6], dados[7]);
+            case COD_STATUS -> dadosFormatados = String.format("""
+                    KP = %s
+                    DATA / HORA = %s
+                    BATERIA = %s
+                    ==== PGM ==== 
+                    %s
+                    === PGM 2 === 
+                    %s
+                    == PARTIÇÃO == 
+                    %s
+                    ELETRIFICADOR = %s
+                    ZONAS = %s
+                    PROBLEMA = %s
+                    PERMISSÕES:
+                    - ELETRIFICADOR = %s
+                    - PGM = %s
+                    - PGM 2 = %s
+                    - PARTIÇÃO = %s
+                    - INIBIR ZONAS = %s
+                    PÂNICO = %s
+                    PARAR SIRENE = %s
+                    ATUALIZAÇÃO = %s""", dados[0], dados[1], dados[2], dados[3], dados[4], dados[5], dados[6], dados[7], dados[8], dados[9], dados[10], dados[11], dados[12], dados[13], dados[14], dados[15], dados[16]);
             default -> {
                 return null;
             }
---
+++ src/main/java/br/com/fulltime/app/model/Status.java
@@ -0,0 +1,4 @@
+package br.com.fulltime.app.model;
+
+public class Status {
+}
