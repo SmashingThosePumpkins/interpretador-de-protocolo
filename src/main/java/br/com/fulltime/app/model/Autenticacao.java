8bab8f0d926624387fdea599d49247579e6bf18c
Pablo A <pablo.silva@ftdata.com.br>
2021-06-10 11:26:43 -0300

    Refatoração no método de autenticação

--- src/main/java/br/com/fulltime/app/model/Autenticacao.java
+++ src/main/java/br/com/fulltime/app/model/Autenticacao.java
@@ -7,14 +7,14 @@ public class Autenticacao implements Comando {
     public String[] getDados(String[] array) {
 
         String numSerie = aplicarNumero(array, 4, 14),
-            imei = aplicarNumero(array, 14, 29),
-            mac = aplicarNumero(array, 29, 41),
-            modelo = aplicarModelo(array, 41),
-            versao = aplicarVersao(array, 42),
-            ip = aplicarIP(array, 45),
-            simCard = aplicarSIMCARD(array, 46),
-            via = aplicarVia(array, 47),
-            operadora = aplicarOperadora(array, 48);
+                imei = aplicarNumero(array, 14, 29),
+                mac = aplicarNumero(array, 29, 41),
+                modelo = aplicarModelo(array, 41),
+                versao = aplicarVersao(array, 42),
+                ip = aplicarIP(array, 45),
+                simCard = aplicarSIMCARD(array, 46),
+                via = aplicarVia(array, 47),
+                operadora = aplicarOperadora(array, 48);
 
 
         return new String[]{numSerie, imei, mac, modelo, versao, ip, simCard, via, operadora};
@@ -25,59 +25,29 @@ public class Autenticacao implements Comando {
         var dados = getDados(array);
         return String.format(
                 """
-                NUMERO DE SERIE = %s
-                IMEI = %s
-                MAC = %s
-                MODELO = %s
-                VERSÃO = %s
-                IP = %s
-                SIMCARD = %s
-                VIA = %s
-                OPERADORA = %s""", dados[0], dados[1], dados[2], dados[3], dados[4], dados[5], dados[6], dados[7], dados[8]);
+                        NUMERO DE SERIE = %s
+                        IMEI = %s
+                        MAC = %s
+                        MODELO = %s
+                        VERSÃO = %s
+                        IP = %s
+                        SIMCARD = %s
+                        VIA = %s
+                        OPERADORA = %s""", dados[0], dados[1], dados[2], dados[3], dados[4], dados[5], dados[6], dados[7], dados[8]);
     }
 
     private String aplicarModelo(String[] array, int index) {
 
-//        return switch (array[index]){
-//            case "A0" -> "ACTIVE 32 DUO";
-//            case "A1" -> "ACTIVE 20 ULTRA";
-//
-//            case "A2" -> {
-//                return "ACTIVE 8 ULTRA";
-//            }
-//            case "A3" -> {
-//                return "ACTIVE 20 ETHERNET";
-//            }
-//            case "A4" -> {
-//                return "ACTIVE 100 BUS";
-//            }
-//            case "A5" -> {
-//                return "ACTIVE 20 BUS";
-//            }
-//            default -> "";
-//        };
+        return switch (array[index]) {
+            case "A0" -> "ACTIVE 32 DUO";
+            case "A1" -> "ACTIVE 20 ULTRA";
+            case "A2" -> "ACTIVE 8 ULTRA";
+            case "A3" -> "ACTIVE 20 ETHERNET";
+            case "A4" -> "ACTIVE 100 BUS";
+            case "A5" -> "ACTIVE 20 BUS";
+            default -> "";
+        };
 
-        switch (array[index]) {
-            case "A0" -> {
-                return "ACTIVE 32 DUO";
-            }
-            case "A1" -> {
-                return "ACTIVE 20 ULTRA";
-            }
-            case "A2" -> {
-                return "ACTIVE 8 ULTRA";
-            }
-            case "A3" -> {
-                return "ACTIVE 20 ETHERNET";
-            }
-            case "A4" -> {
-                return "ACTIVE 100 BUS";
-            }
-            case "A5" -> {
-                return "ACTIVE 20 BUS";
-            }
-            default -> throw new NoSuchElementException();
-        }
     }
 
     private String aplicarVersao(String[] array, int index) {
@@ -95,7 +65,6 @@ public class Autenticacao implements Comando {
     }
 
     private String aplicarNumero(String[] array, int from, int to) {
-//        Arrays.copyOfRange(array, from, to);
         StringBuilder stringBuilder = new StringBuilder();
         do {
             stringBuilder.append((char) Integer.parseInt(array[from], 16));
@@ -105,47 +74,28 @@ public class Autenticacao implements Comando {
     }
 
     private String aplicarIP(String[] array, int index) {
-//        if("01"){
-//            return "IP1";
-//        }
-//        return "IP2";
-
-        switch (array[index]) {
-            case "01" -> {
-                return "IP1";
-            }
-            case "02" -> {
-                return "IP2";
-            }
+        return switch (array[index]) {
+            case "01" -> "IP1";
+            case "02" -> "IP2";
             default -> throw new NoSuchElementException();
-        }
+        };
     }
 
     private String aplicarSIMCARD(String[] array, int index) {
-        switch (array[index]) {
-            case "01" -> {
-                return "SIM1";
-            }
-            case "02" -> {
-                return "SIM2";
-            }
-            case "03" -> {
-                return "N/A";
-            }
+        return switch (array[index]) {
+            case "01" -> "SIM1";
+            case "02" -> "SIM2";
+            case "03" -> "N/A";
             default -> throw new NoSuchElementException();
-        }
+        };
     }
 
     private String aplicarVia(String[] array, int index) {
-        switch (array[index]) {
-            case "00" -> {
-                return "GPRS";
-            }
-            case "01" -> {
-                return "ETHERNET";
-            }
+        return switch (array[index]) {
+            case "00" -> "GPRS";
+            case "01" -> "ETHERNET";
             default -> throw new NoSuchElementException();
-        }
+        };
     }
 
     private String aplicarOperadora(String[] array, int index) {
